1445c995a59b924f4db15c5175b55b84369d635f
Maru Sosa <ms@wayakna.com>
2022-10-06 07:42:47 -0500

    HashSet look for duplicated examples

---
+++ src/Examples/HashSetLookForDuplicatesExample.java
@@ -0,0 +1,21 @@
+package Examples;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetLookForDuplicatesExample {
+    public static void main(String[] args) {
+        String[] fishes = {"Carp", "Bluegill", "Cobia", "Pumpkinseed", "Salmon", "Bluegill", "Salmon"};
+
+        //register this species in a set, detect the duplicates
+
+        Set<String> unique = new HashSet<>();
+
+        for(String fish : fishes) {
+            if(!unique.add(fish)) {
+                System.out.println("Duplicated element: " + fish);
+            }
+        }
+        System.out.println(unique.size() + " not duplicated: " + unique);
+    }
+}
---
+++ src/Examples/HashSetLookForDuplicatesExample2.java
@@ -0,0 +1,24 @@
+package Examples;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetLookForDuplicatesExample2 {
+    public static void main(String[] args) {
+        String[] fishes = {"Carp", "Bluegill", "Cobia", "Pumpkinseed", "Salmon", "Bluegill", "Salmon"};
+
+        //register this species in a set, detect the duplicates
+
+        Set<String> unique = new HashSet<>();
+        Set<String> duplicated = new HashSet<>();
+
+        for(String fish : fishes) {
+            if(!unique.add(fish)) {
+                duplicated.add(fish);
+            }
+        }
+        unique.removeAll(duplicated);
+        System.out.println("Unique: " + unique );
+        System.out.println("Duplicated: " + duplicated);
+    }
+}
